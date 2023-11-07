35d3e60be2d43e23aa0f0fb907d751d8c2d23006
lexmin0412 <zhangle_dev@outlook.com>
2023-11-08 01:37:06 +0800

    refactor: remove useless code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,6 @@
-import { useRequest } from 'ahooks'
-import { Button, message } from 'antd'
-import { Link } from 'react-router-dom'
 import TinyMCEDemo from '../tinymce-demo';
 
 function Home() {
-	const {
-		loading: fetchLoading,
-		runAsync: handleFakeFetch
-	} = useRequest(
-    () => {
-      return fetch(window.location.href).then((res) => {
-        return res.text();
-      });
-    },
-    {
-			manual: true,
-      onSuccess: (res) => {
-        console.log("请求结果", res);
-				message.success('请求成功')
-      },
-    }
-  );
-
   return (
     <div className='p-5'>
 			<div className='text-3xl font-bold mb-4'>TinyMCE 示例</div>
